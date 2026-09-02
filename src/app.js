c9d690258eee3ec20301f28c93446cac1edc78a3
CEARENCINHO <johnmarley126@gmail.com>
2026-09-02 02:43:58 -0400

    Adicioando rota de leitura da tabela

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ import conexaoSQL from './config/dbConnect.js'
 const app = express()
 app.use(express.json())
 
-// app.get('/', (req,res) => {
-//     res.status(200).send('Servidor funcioando!')
-// })
 
 app.post('/leitura', async (req,res) => {
     try {
@@ -38,9 +35,25 @@ app.post('/leitura', async (req,res) => {
 
         const [resultado] = await conexaoSQL.query(sql, valores)
         res.status(201).json({ mensagem: 'Gravado com sucesso' });
+        console.log('Gravado com sucesso')
     } catch (erro) {
         console.error('Erro ao salvar no banco:', erro);
-        res.status(201).json({ mensagem: 'Falha interna ao registrar leitura.' });
+        res.status(500).json({ mensagem: 'Falha interna ao registrar leitura.' });
+        console.log(mensagem, 'Falha interna ao registrar leitura.' )
+    }
+})
+
+
+app.get('/lerTabela', async (req,res) => {
+    
+    try{
+        const sql = 'SELECT * FROM leituras';
+        const [linhas] = await conexaoSQL.query(sql)
+
+            res.status(200).json(linhas)
+    } catch (erro){
+        consolog.log('Erro ao ver a tabela de dados:', erro)
+        res.status(500).json({mensagem: 'Falha a acessar o banco de dados'})
     }
 })
 
